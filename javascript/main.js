57515aa12b2a8c2300473d45f0301ed0f3ef497a
Andrew Brown <andrewtcb@gmail.com>
2017-05-29 14:16:57 +1000

    game logic working

--- javascript/main.js
+++ javascript/main.js
@@ -8,6 +8,49 @@ var ticTacToe = {
               "n","n","n",
               "n","n","n"],
 
+  checkForWin: function(gameboard) {
+    if ( parseInt(this.gameBoard[0]) + parseInt(this.gameBoard[3]) + parseInt(this.gameBoard[6]) === 3) {
+      alert('red wins');
+    } else if ( parseInt(this.gameBoard[0]) + parseInt(this.gameBoard[3]) + parseInt(this.gameBoard[6]) === 0) {
+      alert('blue wins');
+    }
+    if ( parseInt(this.gameBoard[1]) + parseInt(this.gameBoard[4]) + parseInt(this.gameBoard[7]) === 3) {
+      alert('red wins');
+    } else if ( parseInt(this.gameBoard[1]) + parseInt(this.gameBoard[4]) + parseInt(this.gameBoard[7]) === 0) {
+      alert('blue wins');
+    }
+    if ( parseInt(this.gameBoard[2]) + parseInt(this.gameBoard[5]) + parseInt(this.gameBoard[8]) === 3) {
+      alert('red wins');
+    } else if ( parseInt(this.gameBoard[2]) + parseInt(this.gameBoard[5]) + parseInt(this.gameBoard[8]) === 0) {
+      alert('blue wins');
+    }
+    if ( parseInt(this.gameBoard[0]) + parseInt(this.gameBoard[1]) + parseInt(this.gameBoard[2]) === 3) {
+      alert('red wins');
+    } else if ( parseInt(this.gameBoard[0]) + parseInt(this.gameBoard[1]) + parseInt(this.gameBoard[2]) === 0) {
+      alert('blue wins');
+    }
+    if ( parseInt(this.gameBoard[3]) + parseInt(this.gameBoard[4]) + parseInt(this.gameBoard[5]) === 3) {
+      alert('red wins');
+    } else if ( parseInt(this.gameBoard[3]) + parseInt(this.gameBoard[4]) + parseInt(this.gameBoard[5]) === 0) {
+      alert('blue wins');
+    }
+    if ( parseInt(this.gameBoard[6]) + parseInt(this.gameBoard[7]) + parseInt(this.gameBoard[8]) === 3) {
+      alert('red wins');
+    } else if ( parseInt(this.gameBoard[6]) + parseInt(this.gameBoard[7]) + parseInt(this.gameBoard[8]) === 0) {
+      alert('blue wins');
+    }
+    if ( parseInt(this.gameBoard[0]) + parseInt(this.gameBoard[4]) + parseInt(this.gameBoard[8]) === 3) {
+      alert('red wins');
+    } else if ( parseInt(this.gameBoard[0]) + parseInt(this.gameBoard[4]) + parseInt(this.gameBoard[8]) === 0) {
+      alert('blue wins');
+    }
+    if ( parseInt(this.gameBoard[2]) + parseInt(this.gameBoard[4]) + parseInt(this.gameBoard[6]) === 3) {
+      alert('red wins');
+    } else if ( parseInt(this.gameBoard[2]) + parseInt(this.gameBoard[4]) + parseInt(this.gameBoard[6]) === 0) {
+      alert('blue wins');
+    }
+  },
+
   isOdd: function(turnCounter) {
     if (turnCounter % 2) {
       0;
@@ -33,44 +76,78 @@ $( document ).ready(function(){
   var score = function($this) {
     if ($this.is("#tl")) {
       if ($this.is(".red")) {
-        ticTacToe.gameBoard[0] = 1;
-      else if ($this.is(".blue")) {
-          ticTacToe.gameBoard[0] = 1;
+        ticTacToe.gameBoard[0] = "1";
+      } else if ($this.is(".blue")) {
+          ticTacToe.gameBoard[0] = "0";
+        }
       return
     } else if ($this.is("#tm")) {
-
+      if ($this.is(".red")) {
+        ticTacToe.gameBoard[1] = "1";
+      } else if ($this.is(".blue")) {
+          ticTacToe.gameBoard[1] = "0";
+        }
       return
     } else if ($this.is("#tr")) {
-
+      if ($this.is(".red")) {
+        ticTacToe.gameBoard[2] = "1";
+      } else if ($this.is(".blue")) {
+          ticTacToe.gameBoard[2] = "0";
+        }
         return
     } else if ($this.is("#cl")) {
-
+      if ($this.is(".red")) {
+        ticTacToe.gameBoard[3] = "1";
+      } else if ($this.is(".blue")) {
+          ticTacToe.gameBoard[3] = "0";
+        }
         return
     } else if ($this.is("#cm")) {
-
+      if ($this.is(".red")) {
+        ticTacToe.gameBoard[4] = "1";
+      } else if ($this.is(".blue")) {
+          ticTacToe.gameBoard[4] = "0";
+        }
         return
     } else if ($this.is("#cr")) {
-
+      if ($this.is(".red")) {
+        ticTacToe.gameBoard[5] = "1";
+      } else if ($this.is(".blue")) {
+          ticTacToe.gameBoard[5] = "0";
+        }
       return
     } else if ($this.is("#bl")) {
-
+      if ($this.is(".red")) {
+        ticTacToe.gameBoard[6] = "1";
+      } else if ($this.is(".blue")) {
+          ticTacToe.gameBoard[6] = "0";
+        }
         return
     } else if ($this.is("#bm")) {
-
+      if ($this.is(".red")) {
+        ticTacToe.gameBoard[7] = "1";
+      } else if ($this.is(".blue")) {
+          ticTacToe.gameBoard[7] = "0";
+        }
       return
     } else if ($this.is("#br")) {
-
+      if ($this.is(".red")) {
+        ticTacToe.gameBoard[8] = "1";
+      } else if ($this.is(".blue")) {
+          ticTacToe.gameBoard[8] = "0";
+        }
         return
     }
 }
 
   $('.gameBox').on('click', function(){
     var $this = $(this);
-    score($this)
     // var $this = $(this);
     // doubleCheck()
+
     ticTacToe.turnCounter = parseInt(ticTacToe.turnCounter) + 1;
     // console.log(ticTacToe.turnCounter)
+
       if (ticTacToe.isOdd(ticTacToe.turnCounter) == true) {
         $(this).addClass('blue')
         console.log(this.id)
@@ -78,5 +155,9 @@ $( document ).ready(function(){
         $(this).addClass('red')
         console.log(this.id)
       }
+
+    score($this);
+
+    ticTacToe.checkForWin();
     })
   })
